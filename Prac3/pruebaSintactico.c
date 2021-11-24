2bd8f7b476f6004737fb8ff2c6c9f220de840793
dmcarmen <dmcarmen1999@gmail.com>
2021-11-25 00:38:18 +0100

    make all y comentarios bien

--- Prac3/pruebaSintactico.c
+++ Prac3/pruebaSintactico.c
@@ -8,26 +8,26 @@ int main(int argc, char** argv) {
     extern FILE* yyout;
     extern FILE* yyin;
 
-    //Comprobamos que se pasan los argumentos correctamente
+    /* Comprobamos que se pasan los argumentos correctamente */
     if (argc != 3){
         printf("Debe indicarse file de entrada y de salida\n");
         return 1;
     }
 
-    //Leemos el fichero de entrada
+    /* Leemos el fichero de entrada */
     yyin = fopen(argv[1], "r");
     if(yyin == NULL){
         return 1;
     }
 
-    //Creamos el fichera de salida
+    /* Creamos el fichera de salida */
     yyout = fopen(argv[2], "w");
     if(yyout == NULL){
         fclose(yyin);
         return 1;
     }
 
-    //Llamamos al parser
+    /* Llamamos al parser */
     yyparse();
 
     fclose(yyin);
