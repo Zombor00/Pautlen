ffe56c5bf2a4a3419fb725e13b45784aaf725bf2
Zombor <thezombor@gmail.com>
2021-11-25 01:23:54 +0100

    style

--- Prac3/pruebaSintactico.c
+++ Prac3/pruebaSintactico.c
@@ -4,26 +4,30 @@
 int yylex();
 int yyparse();
 
-int main(int argc, char ** argv) {
-    extern FILE * yyout;
-    extern FILE * yyin;
+int main(int argc, char** argv) {
+    extern FILE* yyout;
+    extern FILE* yyin;
 
-    if (argc != 3)
-    {
-        printf("Debe indicar file de entrada y de salida\n");
+    //Comprobamos que se pasan los argumentos correctamente
+    if (argc != 3){
+        printf("Debe indicarse file de entrada y de salida\n");
         return 1;
     }
 
+    //Leemos el fichero de entrada
     yyin = fopen(argv[1], "r");
-    if(yyin == NULL) {
+    if(yyin == NULL){
         return 1;
     }
+
+    //Creamos el fichera de salida
     yyout = fopen(argv[2], "w");
-    if(yyout == NULL) {
+    if(yyout == NULL){
         fclose(yyin);
         return 1;
     }
 
+    //Llamamos al parser
     yyparse();
 
     fclose(yyin);
